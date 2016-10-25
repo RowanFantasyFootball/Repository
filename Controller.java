c398f0822bfb648f0a77da0e0832d42a67c68a30
Joseph Dementri <joedementri42012@gmail.com>
2016-10-25 08:02:39 -0400

    Removed redundant code (local variables taking precedent over class variables)

--- Controller.java
+++ Controller.java
@@ -95,8 +95,6 @@ public class Controller
      */
     public void teamSetUp()
     {        
-        NFLTeam team1 = new NFLTeam("Eagles");
-        NFLTeam team2 = new NFLTeam("Cowboys");
 
         // Each "new String []..." is the stats for each player. TEAM NAME SHOULDN'T BE INCLUDED
         // Player parameters include... Team, Clock, Randomizer, Individual Player Statistics
