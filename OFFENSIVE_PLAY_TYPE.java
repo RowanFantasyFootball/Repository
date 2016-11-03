70e63e60ad67b20e52445b1f0b6b0446bbb74d9a
RowanProf18 <MCCLINTOT4@STUDENTS.ROWAN.EDU>
2016-11-03 08:24:19 -0400

    Update OFFENSIVE_PLAY_TYPE.java

--- OFFENSIVE_PLAY_TYPE.java
+++ OFFENSIVE_PLAY_TYPE.java
@@ -1,11 +1,7 @@
 
 
 /**
- * An enum containing all of the offensive play types
- * 
- * If you are unfamiliar with enums, look up how to use them they are very
- * useful. With enums you have the ability to create your own variable with its
- * own values
+ * enum
  */
 public enum OFFENSIVE_PLAY_TYPE {
 	/**
@@ -26,23 +22,7 @@ public enum OFFENSIVE_PLAY_TYPE {
 
 	/**
 	 * Constructor for creating a new offensive play type enum
-	 * 
-	 * however!!!, This can be subject to change. The potential values are set
-	 * randomly, WE NEED IDEAS if we want to change this!!! So, implement your
-	 * own ideas and upload them to the github so everyone can see. If you feel
-	 * confident with what you've came up with, definitely bring it up to the
-	 * teams, we had really some good ideas in class that I would like to get
-	 * implemented.
-	 * 
-	 * Don't feel bad if you think you have a better idea, because you
-	 * definitely can have a better idea. Implement them (but don't delete old
-	 * code, comment it out so we see what has changed). If it is good, we will
-	 * keep it! Don't worry about neatness or repetetive code. I will fix that
-	 * up for everyone if it becomes a problem, which I don't think it will. The
-	 * code looked good overall before I added some new ideas
-	 * 
-	 * - Joe D
-	 */
+	
 	OFFENSIVE_PLAY_TYPE() {
 		yardageGainPotential = Randomizer.getRandomNumber(DefensivePlay.MAX_STOPPING_POTENTIAL);
 	}
