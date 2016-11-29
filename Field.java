c6e831880aeca546d465367b27e05f1f97de50a5
RowanProf18 <MCCLINTOT4@STUDENTS.ROWAN.EDU>
2016-11-29 08:21:10 -0500

    TM 11292016

--- Field.java
+++ Field.java
@@ -1,59 +1,17 @@
-import java.util.*;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-/**
- * Write a description of class Field here.
- * 
- * @author Clinton Conyer
- * @version 11/21/2016
- */
-public class Field
-{
-    
-    // The depth and width of the field.
-    private int depth, width;
-    private Object[][] field;
+import javax.swing.*;
+import java.awt.*;
 
-    public Field(int depth, int width)
-    {
-        this.depth = depth;
-        this.width = width;
-        field = new Object[depth][width];
-    }
-    
-        public void clear()
-    {
-        for(int row = 0; row < depth; row++) {
-            for(int col = 0; col < width; col++) {
-                field[row][col] = null;
-            }
-        }
-    }
-   
+public class Field extends JPanel {
 
-    /**
-     * Place an player at the given location.
-     * @param animal The animal to be placed.
-     * @param row Row coordinate of the location.
-     * @param col Column coordinate of the location.
-     */
-    public void setLocation(String player, Location Location)
-    {
-        Location newLocation = Location;
-        //field.place(player, newLocation);
+    public Field() {
+        super();
+        initComponents();
     }
     
-    
-    public int getDepth()
-    {
-        return depth;
-    }
-    
-    public int getWidth()
-    {
-        return width;
+    private void initComponents() {
+        this.setLayout(new FlowLayout());
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
+        
+        this.add(new JLabel(new ImageIcon("field.png")));
     }
 }
---
+++ TeamplayerA.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TeamplayerA
+{
+    // instance variables - replace the example below with your own
+    private int number;
+    private String name;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public TeamplayerA(Field field, Location location)
+    {
+        //num = number;
+        //name = name;
+    }
+}
